335f12b75c93697ed54434d58c74ad9ca50752f9
Sean <seanoneill@Bons-MacBook-Pro.local>
2019-02-04 15:10:07 -0800

    working on nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,11 @@ import React, { Component } from "react";
 import "./styles/main.css";
 
 
-class Header extends Component {
-  render(){
-    return (
-      <div className="header">
-      <h1>Tunip The Beet!</h1>
-        <div className="nav">
-          <ul>
-            <li>Home</li>
-            <li>Add a Recipie</li>
-            <li>About</li>
-            <li>Playlists</li>
-          </ul>
-        </div>
-      </div>
-    )
-  }
-}
-
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
       </div>
     );
   }
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect('mongodb://SeanBriar:Meow_1011@ds249123.mlab.com:49123/recipes_
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
 });
 
 app.listen(PORT, () => {
